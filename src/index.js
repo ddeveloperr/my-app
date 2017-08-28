793cf9ac313ac2aeb36425feb14ab12bf94ead43
KemalC <ddeveloperr@gmail.com>
2017-08-28 09:43:00 +0000

    X and O take turns

--- src/index.js
+++ src/index.js
@@ -12,20 +12,39 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+    constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+                   squares: squares,
+                   xIsNext: !this.state.xIsNext,
+    });
+  }  
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square 
+             value={this.state.squares[i]}
+             onClick = {() => this.handleClick(i)}
+            />;
   }
   
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
